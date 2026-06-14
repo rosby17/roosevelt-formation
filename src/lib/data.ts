15a4f75eed8af63b3734ba07f9d35b257b85f475
MAC <MAC@MacBook-Pro.local>
2026-06-14 21:05:26 +0100

    feat: landing page formation YouTube

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -4,7 +4,7 @@ export const SITE = {
   description:
     "Développe et monétise ta chaîne YouTube. 10 modules, avatars IA, stratégie multi-chaînes, sans audience de départ.",
   url: "https://formation.tools-cl.com",
-  heroVideo: "", // Mettre l'URL d'intégration YouTube ici (ex: https://www.youtube.com/embed/...)
+  heroVideo: "https://www.youtube.com/watch?v=3VnxBDg3374&t=200s&themeRefresh=1", // Mettre l'URL d'intégration YouTube ici (ex: https://www.youtube.com/embed/...)
   socials: {
     youtube: "https://www.youtube.com/@rooseveltmogo",
     facebook: "https://www.facebook.com/rooseveltmogo17",
